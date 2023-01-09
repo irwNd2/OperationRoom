7b4c213e4b445395cc5ddeb55b0787aa896aa208
irwNd2 <irwandduadua@gmail.com>
2023-01-09 23:45:14 +0800

    works on all scenarios

--- app.js
+++ app.js
@@ -11,25 +11,23 @@ app.post("/bookingkamaroperasi/:booking/:duration", (req, res) => {
   // Extract the bookingDate and startTime from the booking parameter
   const [bookingDate, startTime] = booking.split("T");
 
-  // Calculate the start and end times for the booking
-  const start = `${bookingDate}T${startTime}`;
-  const endTime = new Date(start);
-  endTime.setHours(endTime.getHours() + parseInt(duration, 10));
+  // Calculate the start and end times for the booking (add 2 hours to the duration to account for setup and cleanup)
+  const date = `${bookingDate}T${startTime}:00.000Z`;
+  const start = new Date(date);
+  const endTime = new Date(date);
+  endTime.setHours(endTime.getHours() + parseInt(duration) + 2);
+  endTime.toISOString();
 
   // Check if the operation room is available during the requested time period
-  const conflicts = bookings.some((booking) => {
-    return (
-      (start >= booking.startTime && start < booking.endTime) ||
-      (endTime > booking.startTime && endTime <= booking.endTime) ||
-      (start <= booking.startTime && endTime >= booking.endTime)
-    );
-  });
+  const conflicts = bookings.some(
+    (booking) => booking.start < endTime && booking.endTime > start
+  );
 
   if (conflicts) {
-    // If there is a conflict, return a 409 Conflict response
+    // If there is a conflict, return a 409 Conflict response and false message
     res.status(409).json({ message: false });
   } else {
-    // If the operation room is available, add the booking to the array and return a 200 OK response
+    // If the operation room is available, add the booking to the array and return a 200 OK response and true message
     bookings.push({ start, endTime });
     res.status(200).json({ message: true });
   }
